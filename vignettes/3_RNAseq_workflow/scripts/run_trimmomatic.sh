0aaf9636c955d347af8c6de4ed4d78e164d00053
CDN-BC <fursham.h@gmail.com>
2021-05-20 12:52:34 +0100

    Added slidingwindow trimming

--- vignettes/3_RNAseq_workflow/scripts/run_trimmomatic.sh
+++ vignettes/3_RNAseq_workflow/scripts/run_trimmomatic.sh
@@ -10,6 +10,6 @@ trimmomatic PE \
     ./originals/FASTQ/SRR7311317_1.fastq.gz ./originals/FASTQ/SRR7311317_2.fastq.gz \
     ./outputs/trimmed_FASTQ/SRR7311317_1.fastqc.gz ./outputs/trimmed_FASTQ/SRR7311317_1.unpaired.fastqc.gz \
     ./outputs/trimmed_FASTQ/SRR7311317_2.fastqc.gz ./outputs/trimmed_FASTQ/SRR7311317_2.unpaired.fastqc.gz \
-    ILLUMINACLIP:./scripts/Nextera_TruSeq-PE.fa:2:30:10:2:keepBothReads MINLEN:40
+    ILLUMINACLIP:./scripts/Nextera_TruSeq-PE.fa:2:30:10:2:keepBothReads SLIDINGWINDOW:4:30 MINLEN:40
 
 echo "Completed....."
