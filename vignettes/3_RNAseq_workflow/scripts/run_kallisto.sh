4baf11611abbdc034f0b79a18ec12117885f9f0c
CDN-BC <fursham.h@gmail.com>
2021-05-20 16:16:21 +0100

    Corrected trimmed filename

--- vignettes/3_RNAseq_workflow/scripts/run_kallisto.sh
+++ vignettes/3_RNAseq_workflow/scripts/run_kallisto.sh
@@ -8,7 +8,7 @@ kallisto quant \
     -i ./originals/transcriptome.idx \
     -o ./outputs/kallisto \
     --rf-stranded \
-    ./outputs/trimmed_FASTQ/SRR7311317_1.trimmed.fastqc.gz \
-    ./outputs/trimmed_FASTQ/SRR7311317_2.trimmed.fastqc.gz
+    ./outputs/trimmed_FASTQ/SRR7311317_1.trimmed.fastq.gz \
+    ./outputs/trimmed_FASTQ/SRR7311317_2.trimmed.fastq.gz
     
 echo "Completed......."
--- vignettes/3_RNAseq_workflow/scripts/run_trimmomatic.sh
+++ vignettes/3_RNAseq_workflow/scripts/run_trimmomatic.sh
@@ -8,8 +8,8 @@ mkdir -p ~/RNAseq_workshop/outputs/trimmed_FASTQ
 
 trimmomatic PE \
     ./originals/FASTQ/SRR7311317_1.fastq.gz ./originals/FASTQ/SRR7311317_2.fastq.gz \
-    ./outputs/trimmed_FASTQ/SRR7311317_1.trimmed.fastqc.gz ./outputs/trimmed_FASTQ/SRR7311317_1.unpaired.fastqc.gz \
-    ./outputs/trimmed_FASTQ/SRR7311317_2.trimmed.fastqc.gz ./outputs/trimmed_FASTQ/SRR7311317_2.unpaired.fastqc.gz \
+    ./outputs/trimmed_FASTQ/SRR7311317_1.trimmed.fastq.gz ./outputs/trimmed_FASTQ/SRR7311317_1.unpaired.fastq.gz \
+    ./outputs/trimmed_FASTQ/SRR7311317_2.trimmed.fastq.gz ./outputs/trimmed_FASTQ/SRR7311317_2.unpaired.fastq.gz \
     ILLUMINACLIP:./scripts/Nextera_TruSeq-PE.fa:2:40:15 SLIDINGWINDOW:4:20 HEADCROP:7 MINLEN:35
 
 echo "Completed....."
